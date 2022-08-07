aaeb248a9663a04fe15c206c203aeb315acb5ce3
Er1212 <erez.katzav@gmail.com>
2022-08-07 20:55:30 +0300

    init commit

---
+++ Test2.py
@@ -0,0 +1,13 @@
+modulo_variable = 14 % 4
+print(modulo_variable)
+
+cool_number = 12 + 30
+cool_number * 5
+print(cool_number)
+
+exponented_variable = 2 ** 4
+print(exponented_variable)
+
+total_cost = 5
+total_cost += 10
+print(total_cost)
